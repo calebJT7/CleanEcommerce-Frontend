337c4fecfdeb24e207f4fbf75607243ba4eadf1c
calebJT7 <calebsingularity@gmail.com>
2026-06-11 13:17:31 -0300

    fix axios

--- services/api.ts
+++ services/api.ts
@@ -1,14 +1,12 @@
 import axios from 'axios';
 
-// Creamos una instancia centralizada de Axios
 export const api = axios.create({
-    // TODO: Cambia este puerto por el que usa tu API en .NET al hacer 'dotnet run'
-    baseURL: 'http://localhost:5002/api', 
+    // Vercel buscará esta variable. Si no la encuentra, usa la de Azure como respaldo
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://api-caleb-ecommerce-fzbqhjhhhufzcybp.centralus-01.azurewebsites.net/',
     headers: {
         'Content-Type': 'application/json',
     },
 });
-
 // Interceptor: Antes de que salga cualquier petición, le inyectamos el Token JWT si existe
 api.interceptors.request.use((config) => {
     // En Next.js (lado del cliente), leemos el token de localStorage
